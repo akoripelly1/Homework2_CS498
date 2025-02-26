ef30d486a1e74369a963e3c2f007d3e461851616
Ashika Koripelly <ashika.koripelly@gmail.com>
2025-02-26 06:09:26 +0000

    Updated instance 2 configuration

--- instance-1/server.js
+++ instance-1/server.js
@@ -3,7 +3,7 @@ const mariadb = require('mariadb');
 const path = require('path');
 
 const app = express();
-const port = 80;
+const port = 8080;
 
 const pool = mariadb.createPool({
     host: '127.0.0.1',
